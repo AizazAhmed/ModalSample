a26d23f813488135bf84f3b6004b4cd3e77e1bca
Andrew Pattison <andrew.pattison@brightfuture.co.uk>
2015-08-05 08:13:43 +0100

    Initial commit.

---
+++ ModelSample/Controllers/PopUpController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModelSample.Controllers
+{
+    public class PopUpController : Controller
+    {
+        // GET: PopUp
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PopUp(/*MyModel model*/)
+        {
+            // Call a service and set a member on the model with the result
+            return PartialView("_PopUp"/*, model*/);
+        }
+
+        [HttpPost]
+        public ActionResult PopUp(MyModel model)
+        {
+            return RedirectToAction("Register", "Account");
+        }
+    }
+
+    public class MyModel
+    {
+        public int Number { get; set; }
+    }
+}
---
+++ ModelSample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ModelSample.Startup))]
+namespace ModelSample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
